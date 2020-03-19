bc74d625c0dd5ecf1bf1b8c41ab7d3a78428d7b0
raushan606 <raushankumar606@gmail.com>
2020-03-20 00:44:44 +0530

    added forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,7 +18,8 @@ const forecast = (latitude, longitude, callback) => {
       } else if (body.error) {
         callback("Unable to find loaction", undefined);
       } else {
-        callback(undefined, body.daily.data[0].summary+' '+body.currently.temperature+' degree.');
+        console.log(body.daily.data[0])
+        callback(undefined, body.daily.data[0].summary+' '+'It is currentyl: '+body.currently.temperature+' degree out. '+ 'This is high today is '+body.daily.data[0].temperatureHigh+' with a low of '+body.daily.data[0].temperatureLow+'. There is a ' + body.currently.precipProbability+' rain probability.');
       }
     }
   );
